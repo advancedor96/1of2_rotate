74b605887470feef446fad6758854c296a883230
Ding <advancedor96@gmail.com>
2025-04-27 17:25:35 +0200

    完成pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '轉盤應用程式',
+        short_name: '轉盤',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          { src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
 })
